a2f5ab40a17f035a3c3a01cc2d33e1cca7c3f0d7
mistmr7 <michael.mistarz@gmail.com>
2018-09-07 16:36:30 -0400

    commit

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,15 @@
+// Get dependencies
 let path = require('path')
 
+// Use app
 module.exports = function(app){
 
+    // redirect to the home page
     app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname + "../../public/home.html"));
     })
 
+    // Redirect to the survey page
     app.get('/survey', (req, res) => {
         res.sendFile(path.join(__dirname, '/../public/survey.html'))
     })
